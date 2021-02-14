9335da012adcb577970e56050ab73e451483f24f
David Sterry <sterry.david@gmail.com>
2021-02-14 12:48:27 -0800

    set up synth with keys on screen
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 // import logo from './logo.svg';
 import './App.css';
-import Synth from './components/synth';
 import React from 'react';
+import * as Tone from 'tone';
 
-function App() {
+export default function App() {
+  const synth = new Tone.Synth().toDestination();
+
+  function playNote(note) {
+    synth.triggerAttackRelease(`${note}4`, "8n");
+  }
   return (
-    <Synth/>
+    <div className="App">
+      <div className="note-wrapper">
+        <button className="note" onClick={() => playNote("C")}>
+          C
+        </button>
+        <button className="note" onClick={() => playNote("D")}>
+          D
+        </button>
+        <button className="note" onClick={() => playNote("E")}>
+          E
+        </button>
+        <button className="note" onClick={() => playNote("G")}>
+          G
+        </button>
+        <button className="note" onClick={() => playNote("A")}>
+          A
+        </button>
+      </div>
+    </div>
   );
 }
-
-export default App;
+// export default App;
--- src/components/synth.js
+++ src/components/synth.js
@@ -1,19 +1,19 @@
-import * as Tone from "tone";
-import React from 'react';
+// import * as Tone from "tone";
+// import React from 'react';
 
-export default function Synth(){
-  const synth = new Tone.Synth().toDestination();
-  const now = Tone.now()
-  synth.triggerAttack("C4", now)
-  synth.triggerRelease(now + 1)
-  return(
-    <>
-      <h1>placeholder</h1>
-    </>
-  )
-}
+// export default function Synth(){
+//   const synth = new Tone.Synth().toDestination();
+//   const now = Tone.now()
+//   synth.triggerAttack("C4", now)
+//   synth.triggerRelease(now + 1)
+//   return(
+//     <>
+//       <h1>placeholder</h1>
+//     </>
+//   )
+// }
 
 
 
-// event listener to trigger, Tone.start
-// Tone.start() returns a promise, the audio will be ready only after that promise is resolved. 
+// // event listener to trigger, Tone.start
+// // Tone.start() returns a promise, the audio will be ready only after that promise is resolved. 
